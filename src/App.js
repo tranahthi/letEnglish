d81b55e6f550ea8e718454c8fa88faf8103e39ac
nthinhh <104981351+nthinhh@users.noreply.github.com>
2023-10-22 15:09:25 +0700

    Update...

--- src/App.js
+++ src/App.js
@@ -3,6 +3,19 @@ import './App.css';
 import Header from './components/layout/header/Header';
 import Home from './pages/home/Home';
 import ListVideo from './pages/video/ListVideo';
+import Introduce from './pages/introduce/Introduce';
+import Login from './pages/login/Login';
+import Signup from './pages/signup/Signup';
+import ForgetPassword from './pages/forgetpass/ForgetPass';
+import Topic from './pages/topic/Topic';
+import ModalTopic from './pages/modaltopic/ModalTopic';
+import DetailTopic from './pages/detailtopic/DetailTopic';
+
+
+
+
+
+
 
 
 function App() {
@@ -12,7 +25,17 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home />}></Route>
+        <Route path='/introduce' element={<Introduce />}></Route>
+        <Route path='/login' element={<Login />}></Route>
+        <Route path='/signup' element={<Signup />}></Route>
+        <Route path='/forgetpass' element={<ForgetPassword />}></Route>
+        <Route path='/topic' element={<Topic />}></Route>
+        <Route path='/modaltopic' element={<ModalTopic />}></Route>
+        <Route path='/detailtopic' element={<DetailTopic />}></Route>
+
+
         <Route path='/listvideo' element={<ListVideo />}></Route>
+
       </Routes>
     </BrowserRouter>
     </>
--- src/components/layout/header/Header.jsx
+++ src/components/layout/header/Header.jsx
@@ -61,3 +61,73 @@ function Header() {
 
 }
 export default Header
+
+
+// function Header() {
+//     return (
+//         <nav class="navbar navbar-expand-lg fixed-top bg-body-tertiary">
+//             <div class="container-fluid">
+
+//                 <a class="navbar-brand" id="logo" href="#"><img src="/assets/image/logo4.png" /></a>
+
+//                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+//                     <span class="navbar-toggler-icon"></span>
+//                 </button>
+
+//                 <div class="collapse navbar-collapse" id="navbarScroll">
+//                     <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style={{ "--bs-scroll-height": "100px" }}>
+//                         <li class="nav-item">
+//                             <a class="nav-link active" aria-current="page" href="#">Home</a>
+//                         </li>
+
+//                         <li class="nav-item dropdown">
+//                             <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+//                                 Vocabulary
+//                             </a>
+//                             <ul class="dropdown-menu">
+//                                 <li><a class="dropdown-item" href="#">Learn</a></li>
+//                                 <li><a class="dropdown-item" href="#">Practice</a></li>
+//                             </ul>
+//                         </li>
+
+//                         <li class="nav-item">
+//                             <a class="nav-link" href="#">Exam</a>
+//                         </li>
+
+//                         <li class="nav-item">
+//                             <a class="nav-link" href="/listvideo">Video</a>
+//                         </li>
+
+//                     </ul>
+
+//                     <form class="d-flex" role="user_actions">
+
+//                         <div class="btn-group">
+//                             <button type="button" class="btn btn-secondary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+//                             <img className="user_icon" src="/assets/icon/user-regular.svg" alt="Icon" />
+//                             </button>
+//                             <ul class="dropdown-menu dropdown-menu-end">
+//                                 <li><a class="dropdown-item" href="#">Proflie</a></li>
+//                                 <li><a class="dropdown-item" href="#">Log out</a></li>
+//                             </ul>
+//                         </div>
+
+//                         {/* <li class="nav-item dropdown">
+//                             <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+//                             <img  className="user_icon" src="/assets/icon/user-regular.svg" alt="Icon" />
+//                             </a>
+//                             <ul class="dropdown-menu">
+//                                 <li><a class="dropdown-item" href="#">Profile</a></li>
+//                                 <li><a class="dropdown-item" href="#">Log out</a></li>
+//                             </ul>
+//                         </li> */}
+
+//                     </form>
+//                 </div>
+//             </div>
+//         </nav>
+//     );
+// }
+
+
+// export default Header;
